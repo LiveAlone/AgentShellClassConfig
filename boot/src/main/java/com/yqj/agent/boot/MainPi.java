8a4f8200bffe82844c5852fd28e7bc26b80f9444
qijunyao <qijunyao@xiaohongshu.com>
2019-12-20 16:44:49 +0800

    change cal

--- boot/src/main/java/com/yqj/agent/boot/MainPi.java
+++ boot/src/main/java/com/yqj/agent/boot/MainPi.java
@@ -12,17 +12,40 @@ import java.util.concurrent.ThreadLocalRandom;
 public class MainPi {
 
     public static void main(String[] args) {
+        for (int threadIndex = 0; threadIndex < 15; threadIndex++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        double value =  singleCal();
+                        System.out.println("calculate value count is " + value);
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        System.out.println("error cause : " + e.toString());
+                        try {
+                            Thread.sleep(10000);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+
+    private static double singleCal() throws Exception{
+        System.out.println(System.currentTimeMillis());
         long count = 0;
-        long total = 100000000000L;
+        long total = 360000000000L;
         for (long i=0; i<total; i++){
             double x = ThreadLocalRandom.current().nextDouble(1);
             double y = ThreadLocalRandom.current().nextDouble(1);
             if (Math.sqrt(x * x + y * y) < 1.0){
                 count+=1;
             }
+            Thread.sleep(1);
         }
         double pi = 4 * (double)count / (double) total;
-        System.out.println("Pi is: " + pi);
+        return pi;
     }
-
 }
